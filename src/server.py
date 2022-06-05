a51c3229d82a092e2a0c15c63c957cb9d272369e
Alejandro Navarro Bonilla <alejandro.navarro.bonilla@est.una.ac.cr>
2022-06-05 17:02:50 -0600

    ajustes sync y colas

--- src/server.py
+++ src/server.py
@@ -1,5 +1,4 @@
-# This is server code to send video and audio frames over UDP/TCP
-
+import random
 import cv2, imutils, socket
 import numpy as np
 import time
@@ -8,18 +7,16 @@ import threading, wave, pyaudio,pickle,struct
 import sys
 import queue
 import os
-# For details visit pyshine.com
 q = queue.Queue(maxsize=10)
 
 filename =  'video.mp4'
-command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename,'temp.wav')
-os.system(command)
+# command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename,'temp.wav')
+# os.system(command)
 
 BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-host_name = socket.gethostname()
-host_ip = '192.168.8.104'#  socket.gethostbyname(host_name)
+host_ip = '192.168.8.102'
 print(host_ip)
 port = 9688
 socket_address = (host_ip,port)
@@ -28,10 +25,9 @@ print('Listening at:',socket_address)
 
 vid = cv2.VideoCapture(filename)
 FPS = vid.get(cv2.CAP_PROP_FPS)
-global TS
-TS = (0.5/FPS)
+
 BREAK=False
-print('FPS:',FPS,TS)
+print('FPS:',FPS)
 totalNoFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 durationInSeconds = float(totalNoFrames) / float(FPS)
 d=vid.get(cv2.CAP_PROP_POS_MSEC)
@@ -53,70 +49,47 @@ def video_stream_gen():
 	
 
 def video_stream():
-    global TS
-    fps,st,frames_to_count,cnt = (0,0,1,0)
-    cv2.namedWindow('TRANSMITTING VIDEO')        
-    cv2.moveWindow('TRANSMITTING VIDEO', 10,30) 
     while True:
         msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
         print('GOT connection from ',client_addr)
-        WIDTH=400
         
         while(True):
             frame = q.get()
             encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
             message = base64.b64encode(buffer)
             server_socket.sendto(message,client_addr)
-            frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-            if cnt == frames_to_count:
-                try:
-                    fps = (frames_to_count/(time.time()-st))
-                    st=time.time()
-                    cnt=0
-                    if fps>FPS:
-                        TS+=0.001
-                    elif fps<FPS:
-                        TS-=0.001
-                    else:
-                        pass
-                except:
-                    pass
-            cnt+=1
-            
-            
-            
-            cv2.imshow('TRANSMITTING VIDEO', frame)
-            key = cv2.waitKey(int(1000*TS)) & 0xFF	
-            if key == ord('q'):
-                os._exit(1)
-                TS=False
-                break	
+            time.sleep(0.018)
+
                 
 
 def audio_stream():
-    s = socket.socket()
-    s.bind((host_ip, (port-1)))
+    BUFF_SIZE = 65536
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 
-    s.listen(5)
-    CHUNK = 1024
-    wf = wave.open("temp.wav", 'rb')
+    server_socket.bind((host_ip, (port-1)))
+    CHUNK = 10*1024
+    wf = wave.open("temp.wav")
     p = pyaudio.PyAudio()
-    print('server listening at',(host_ip, (port-1)))
+    print('server listening at',(host_ip, (port)),wf.getframerate())
     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                     channels=wf.getnchannels(),
                     rate=wf.getframerate(),
                     input=True,
                     frames_per_buffer=CHUNK)
 
-    client_socket,addr = s.accept()
-
+    data = None
+    sample_rate = wf.getframerate()
     while True:
-        if client_socket:
-            while True:
-                data = wf.readframes(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q",len(a))+a
-                client_socket.sendall(message)
+        msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+        print('GOT connection from ',client_addr,msg)
+        
+        while True:
+            data = wf.readframes(CHUNK)
+            server_socket.sendto(data,client_addr)
+            time.sleep(0.8*CHUNK/sample_rate)
+
+            print(0.8*CHUNK/sample_rate)
                 
 
 from concurrent.futures import ThreadPoolExecutor
