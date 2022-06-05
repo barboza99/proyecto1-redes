9c936603c4084e4d82a5756ea7dcbb4a0f7e9862
barboza99 <jose.barboza.estrada@est.una.ac.cr>
2022-06-05 10:18:11 -0600

    add... codigos de prueba

---
+++ client/prueba.py
@@ -0,0 +1,27 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.videoplayer import VideoPlayer
+
+class MainApp(MDApp):
+	title = "Simple Video"
+	def build(self):
+		self.theme_cls.theme_style = "Dark"
+		self.theme_cls.primary_palette = "BlueGray"
+        
+        # Create videoPlayer Instance
+		player = VideoPlayer(source="video.mp4")
+	
+		# Assign VideoPlayer State
+		player.state = 'play'
+
+		# Set options
+		player.options = {'eos': 'loop'}
+
+		# Allow stretch
+		player.allow_stretch = True
+
+		# Return player
+		return player
+
+
+MainApp().run()
--- main.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-import tkinter
-
-root = Tk()
-root.title("VENTANA DE PRUEBA")
-root.resizable(600,600)
-
-root.mainloop()
